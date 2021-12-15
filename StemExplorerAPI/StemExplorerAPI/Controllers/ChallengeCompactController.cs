bd1a470223bbbf7938653723d3ed07a991c44069
Michael Forde <michael@swipedon.com>
2021-12-15 20:12:20 +1300

    Initial work to setup first endpoint to return ChallengeCompacts

---
+++ StemExplorerAPI/StemExplorerAPI/Controllers/ChallengeCompactController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StemExplorerAPI.Models.ViewModels;
+using StemExplorerAPI.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StemExplorerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChallengeCompactController : ControllerBase
+    {
+        private readonly IChallengeCompactService _challengeCompactService;
+
+        public ChallengeCompactController(IChallengeCompactService challengeCompactService)
+        {
+            _challengeCompactService = challengeCompactService;
+        }
+
+        // Return all with optional filter for category type
+        [HttpGet]
+        public async Task<IEnumerable<ChallengeCompactDto>> GetChallengeCompacts()
+        {
+            return await _challengeCompactService.GetChallengeCompactsAsync();
+        }
+
+
+        // Return by id
+    }
+}
---
+++ StemExplorerAPI/StemExplorerAPI/Services/ChallengeCompactService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using StemExplorerAPI.Models;
+using StemExplorerAPI.Models.ViewModels;
+using StemExplorerAPI.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StemExplorerAPI.Services
+{
+    public class ChallengeCompactService : IChallengeCompactService
+    {
+        private readonly StemExplorerContext _context;
+
+        public ChallengeCompactService(StemExplorerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ChallengeCompactDto>> GetChallengeCompactsAsync()
+        {
+            return await _context.ChallengeCompacts.Select(c => new ChallengeCompactDto
+            {
+                Id = c.Id,
+                Title = c.Title,
+                Category = c.Category,
+                Question = c.Question,
+                Instructions = c.Instructions,
+                Hint = c.Hint,
+                PossibleAnswers = JsonConvert.DeserializeObject<List<PossibleAnswer>>(c.PossibleAnswers),
+                Answer = c.Answer,
+                AnswerBlurb = c.AnswerBlurb,
+                IsComplete = c.IsComplete,
+                Position = new Position
+                {
+                    Latitude = c.Latitude,
+                    Longitude = c.Longitude
+                }
+            }).ToListAsync();
+        }
+    }
+}
