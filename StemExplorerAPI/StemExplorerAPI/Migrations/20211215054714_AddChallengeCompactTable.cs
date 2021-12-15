a410c524a5d3fb32a0a96cd3db476289f89fb278
Michael Forde <michael@swipedon.com>
2021-12-15 18:53:30 +1300

    Added ChallengeCompact table to data context

---
+++ StemExplorerAPI/StemExplorerAPI/Migrations/20211215054714_AddChallengeCompactTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace StemExplorerAPI.Migrations
+{
+    public partial class AddChallengeCompactTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChallengeCompacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Category = table.Column<int>(nullable: false),
+                    Question = table.Column<string>(nullable: true),
+                    Instructions = table.Column<string>(nullable: true),
+                    Hint = table.Column<string>(nullable: true),
+                    PossibleAnswers = table.Column<string>(nullable: true),
+                    Answer = table.Column<int>(nullable: false),
+                    AnswerBlurb = table.Column<string>(nullable: true),
+                    IsComplete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChallengeCompacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChallengeCompacts");
+        }
+    }
+}
---
+++ StemExplorerAPI/StemExplorerAPI/Models/Entities/ChallengeCompact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StemExplorerAPI.Models.Entities
+{
+    public class ChallengeCompact
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Enums.ChallengeCategories Category { get; set; }
+        public string Question { get; set; }
+        public string Instructions { get; set; }
+        public string Hint { get; set; }
+        public string PossibleAnswers { get; set; } // json string
+        public int Answer { get; set; }
+        public string AnswerBlurb { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
